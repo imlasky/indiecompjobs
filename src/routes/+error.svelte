3b91012d39f117a3d6a5b02b87e387b5931a3f8f
Ian Lasky <ianmlasky@gmail.com>
2023-01-05 13:17:42 -0500

    added error page, removed navbar

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-center">
+    <div class="flex flex-col justify-center">
+        <h1 class="text-3xl">Unable to find that page</h1>
+        <div class="flex justify-center">
+            <a href="/" class="btn bg-base-100 normal-case">Click here to go home</a>
+
+        </div>
+    </div>
+    
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
     import Navbar from "$lib/nav/Navbar.svelte";
 </script>
 <div class="bg-gradient-to-r from-[#324a85] to-[#6be5ef] h-screen">
-    <Navbar/>
+    <!-- <Navbar/> -->
     <slot />
 </div>
